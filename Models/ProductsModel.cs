abd1be09a41f2939ce1eb62cf0bb424da7eb5d01
Cristobal Garcia <cristobal.Garcia@rsmus.com>
2024-03-02 11:18:22 -0600

    Added latest methods

---
+++ Models/ProductsModel.cs
@@ -0,0 +1,8 @@
+
+public class ProductsModel {
+    public int ProductID {get;set;}
+    public string? Name {get;set;}
+    public string? ProductModel {get;set;}
+    public string? CultureID {get;set;}
+    public string? Description {get;set;}
+}
