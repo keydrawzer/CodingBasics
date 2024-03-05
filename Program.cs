3b8bfdbe6856cf8738f9bcbdc065e847f40bac29
Billyval12 <00124621@uca.edu.sv>
2024-03-04 19:06:07 -0600

    Add: Person endpoints

--- Program.cs
+++ Program.cs
@@ -1,9 +1,64 @@
+using CodingBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+// implement the dependency injection
+builder.Services.AddDbContext<AdventureWorksDbContext>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+// This is the endpoint that will return the list of employees
+app.MapGet("/person", async(AdventureWorksDbContext dbContext) =>
+{
+    var employees = await dbContext.VEmployees
+        .OrderBy(e => e.BusinessEntityId)
+        .ToListAsync();
+
+    return employees;
+});
+
+app.MapGet("/person/GetByName", ([FromQuery] string name, AdventureWorksDbContext dbContext) =>
+{
+    try
+    {
+        var persons = dbContext.VEmployees
+            .AsEnumerable()
+            .Where(e => $"{e.FirstName} {e.MiddleName} {e.LastName}".Contains(name, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+
+        return Results.Json(persons);
+    }
+    catch (Exception ex)
+    {
+        return Results.Json($"Error: {ex.Message}");
+    }
+});
+
+app.MapGet("/persons/byPersonType", (AdventureWorksDbContext dbContext, [FromQuery] string personType) =>
+{
+    try
+    {
+        var results = dbContext.VEmployees
+            .FromSqlRaw($"SELECT * FROM HumanResources.vEmployee WHERE BusinessEntityID IN (SELECT BusinessEntityID FROM Person.Person WHERE PersonType = {personType})")
+            .ToList();
+
+        return Results.Json(results);
+    }
+    catch (Exception ex)
+    {
+        return Results.Json($"Error: {ex.Message}");
+    }
+});
+
+
+
+
+
 
 
 
