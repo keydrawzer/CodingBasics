3015173874c72c05daa0c45aad2998a6b9bce8d1
Ernesto Hernandez <ernesto1991diaz@gmail.com>
2024-03-05 22:31:17 -0600

    chore: done with PersonController.cs

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -25,4 +25,37 @@ public class PersonController : ControllerBase
         }
         return Ok(people);
     }
+
+    [HttpGet("{name}")]
+    public IActionResult GetPersonByName(string name)
+    {
+        var people = _personService.GetPersonByName(name);
+        if (people == null || people.Count == 0)
+        {
+            return NotFound($"No people found with the name '{name}'.");
+        }
+        return Ok(people);
+    }
+
+     [HttpGet("{type}")]
+    public IActionResult GetPersonType(string type)
+    {
+        var people = _personService.GetPersonByPersonType(type);
+        if (people == null || people.Count == 0)
+        {
+            return NotFound($"No people found with the type '{type}'.");
+        }
+        return Ok(people);
+    }
+
+        [HttpGet("{name}/{personType}")]
+    public IActionResult GetPersonByNameAndPersonType(string name, string personType)
+    {
+        var people = _personService.GetPersonByNameAndPersonType(name, personType);
+        if (people == null || people.Count == 0)
+        {
+            return NotFound($"No people found with the name '{name}' and person type '{personType}'.");
+        }
+        return Ok(people);
+    }
 }
