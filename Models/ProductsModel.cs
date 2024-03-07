bc8538c237f308a36cbc40de929ce3786eb3a466
oscar <oscar.martz511@gmail.com>
2024-03-07 12:53:54 -0600

    Create the products model class

---
+++ Models/ProductsModel.cs
@@ -0,0 +1,13 @@
+public class ProductsModel
+{
+    public int ProductID {get;set;}
+    public string? Name {get;set;}
+    public string? ProductNumber {get;set;}
+    public string? Color {get;set;}
+    public decimal? StandardCost {get;set;}
+    public decimal? ListPrice {get;set;}
+    public string? Size {get;set;}
+    public decimal? Weight {get;set;}
+    public string? Class {get;set;}
+    public string? Style {get;set;}
+}
