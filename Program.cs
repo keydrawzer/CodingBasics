006bccde3854589e28cb09ab0b5d2cedfcbdf9b6
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-03-07 12:58:34 -0600

    Add support for Swagger

--- Program.cs
+++ Program.cs
@@ -3,16 +3,20 @@ using CodingBasics.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AdventureWorks2022Context>(options => 
+builder.Services.AddDbContext<AdventureWorks2022Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("localServer"))
 );
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
