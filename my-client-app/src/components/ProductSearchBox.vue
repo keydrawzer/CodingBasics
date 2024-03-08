d9d7bfd983ec06c5b4a81eaf41e1c794b51ad193
Daniel Bonilla <daniel.elias7@gmail.com>
2024-03-07 20:34:30 -0600

    Finish challenge

---
+++ my-client-app/src/components/ProductSearchBox.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="search-container">
+        <div class="search-group">
+            <label for="nameSearch">Search by Name:</label>
+            <input
+                v-model="state.searchText"
+                @change="() => $emit('searchByName', state.searchText)"
+                placeholder="Enter name"
+                class="search-input box-shadow"
+                id="nameSearch"
+            />
+        </div>
+        <div class="search-group">
+            <label for="categorySearch">Search by Product Category Type:</label>
+            <select
+                v-model="state.selectedCategory"
+                @change="() => $emit('searchByCategory', state.selectedCategory)"
+                class="search-select box-shadow"
+                id="categorySearch"
+            >
+                <option value="">None</option>
+                <option value="Bikes">Bikes</option>
+                <option value="Components">Components</option>
+                <option value="Clothing">Clothing</option>
+                <option value="Accesories">Accesories</option>
+            </select>
+        </div>
+    </div>
+</template>
+<script setup>
+    import { reactive } from "vue";
+
+    const state = reactive({
+        searchText: "",
+        selectedCategory: "",
+    });
+</script>
+<style>
+    .search-container {
+        margin-bottom: 20px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .search-container label {
+        margin-right: 15px;
+    }
+
+    .search-input,
+    .search-select {
+        font-size: 16px;
+        font-family: "Roboto", sans-serif;
+        padding: 10px;
+        margin-right: 10px;
+        border-radius: 4px;
+        border: 1px solid #ddd;
+    }
+
+    .search-input {
+        width: 400px;
+    }
+</style>
--- my-client-app/src/pages/ProductsPage.vue
+++ my-client-app/src/pages/ProductsPage.vue
@@ -1,2 +1,79 @@
-<template><div></div></template>
-<script setup></script>
+<template>
+    <div class="product-page">
+        <loader-comp :loading="state.loading" />
+        <h1>List of Product</h1>
+        <search-box
+            @searchByName="searchByName"
+            @searchByCategory="searchByCategory"
+        />
+        <product-table :products="state.products" />
+    </div>
+</template>
+
+<script setup>
+    import { onMounted, reactive } from "vue";
+    import ProductTable from "@/components/ProductTable.vue";
+    import SearchBox from "@/components/ProductSearchBox.vue";
+    import axios from "/src/utils/axios.js";
+    import LoaderComp from "@/components/LoaderComp.vue";
+  
+    const state = reactive({
+        loading: false,
+        products: [],
+    });
+  
+    async function fetchData() {
+        try {
+            state.loading = true;
+            const response = await axios.get("/product");
+            state.products = response.data;
+            state.products.sort((a, b) => a.productID - b.productID);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            state.loading = false;
+        }
+    }
+  
+    async function searchByName(name) {
+        try {
+            state.loading = true;
+            const response = await axios.get(
+                `/product/GetByName/?name=${name}`
+            );
+            state.products = response.data;
+        } catch (error) {
+            console.error(error);
+        } finally {
+            state.loading = false;
+        }
+    }
+
+    async function searchByCategory(category) {
+        try {
+            state.loading = true;
+            const response = await axios.get(
+                `/product/GetByCategoryType/?category=${category}`
+            );
+            state.products = response.data;
+        } catch (error) {
+            console.error(error);
+        } finally {
+            state.loading = false;
+        }
+    }
+  
+    onMounted(async () => {
+        await fetchData();
+    });
+</script>
+<style scoped>
+    .product-page {
+        width: 75%;
+        margin-top: 80px;
+        margin-bottom: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+</style>
