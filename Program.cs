863068e869cf4a9a845b39e8a8e1f3fa6fdf1237
Ernesto Hernandez <ernesto1991diaz@gmail.com>
2024-03-07 19:10:45 -0600

    feat: add swagger for documentation available at http://localhost:5238/swagger/index.html

--- Program.cs
+++ Program.cs
@@ -4,9 +4,9 @@ using CodingBasics.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddSingleton<DataClient>()
@@ -25,11 +25,9 @@ builder.Services
 
 var app = builder.Build();
 
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
