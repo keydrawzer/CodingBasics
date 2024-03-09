07f8540fbfc222c4d4be35865be3b8503267c86f
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-03-08 20:19:14 -0600

    Refactor to make it more reusable

--- my-client-app/src/components/SearchBox.vue
+++ my-client-app/src/components/SearchBox.vue
@@ -1,68 +1,60 @@
 <template>
 	<div class="search-container">
 		<div class="search-group">
-			<label for="nameSearch">Search by Name:</label>
-			<input
-				v-model="state.searchText"
-				@change="
-					() => $emit('searchByName', state.searchText, state.selectedType)
-				"
-				placeholder="Enter name"
-				class="search-input box-shadow"
-				id="nameSearch"
-			/>
+			<label for="textSearch">{{ props.searchLabelText }}</label>
+			<input v-model="state.searchText" @change="() => $emit('searchByName', state.searchText, state.selectedOption)
+				" :placeholder="props.placeholderText" class="search-input box-shadow" id="textSearch" />
 		</div>
 		<div class="search-group">
-			<label for="typeSearch">Search by Person Type:</label>
-			<select
-				v-model="state.selectedType"
-				@change="
-					() => $emit('searchByType', state.searchText, state.selectedType)
-				"
-				class="search-select box-shadow"
-				id="typeSearch"
-			>
-				<option value="">None</option>
-				<option value="IN">IN</option>
-				<option value="EM">EM</option>
-				<option value="SP">SP</option>
-				<option value="SC">SC</option>
-				<option value="VC">VC</option>
-				<option value="GC">GC</option>
+			<label for="optionsFilter">{{ props.selectLabelText }}</label>
+			<select v-model="state.selectedOption" @change="() => $emit('filterByOption', state.searchText, state.selectedOption)
+				" class="search-select box-shadow" id="optionsFilter">
+				<option v-for="option in props.selectOptions" :value="option.value" :key="option.value">{{ option.label }}
+				</option>
 			</select>
 		</div>
 	</div>
 </template>
 <script setup>
-	import { reactive } from "vue";
+import { defineProps, reactive } from 'vue';
 
-	const state = reactive({
-		searchText: "",
-		selectedType: "",
-	});
+const props = defineProps({
+	searchLabelText: String,
+	selectLabelText: String,
+	placeholderText: {
+		type: String,
+		default: 'Enter name'
+	},
+	selectOptions: Array
+});
+
+const state = reactive({
+	searchText: "",
+	selectedOption: "",
+});
 </script>
 <style>
-	.search-container {
-		margin-bottom: 20px;
-		display: flex;
-		justify-content: center;
-	}
+.search-container {
+	margin-bottom: 20px;
+	display: flex;
+	justify-content: center;
+}
 
-	.search-container label {
-		margin-right: 15px;
-	}
+.search-container label {
+	margin-right: 15px;
+}
 
-	.search-input,
-	.search-select {
-		font-size: 16px;
-		font-family: "Roboto", sans-serif;
-		padding: 10px;
-		margin-right: 10px;
-		border-radius: 4px;
-		border: 1px solid #ddd;
-	}
+.search-input,
+.search-select {
+	font-size: 16px;
+	font-family: "Roboto", sans-serif;
+	padding: 10px;
+	margin-right: 10px;
+	border-radius: 4px;
+	border: 1px solid #ddd;
+}
 
-	.search-input {
-		width: 400px;
-	}
+.search-input {
+	width: 400px;
+}
 </style>
